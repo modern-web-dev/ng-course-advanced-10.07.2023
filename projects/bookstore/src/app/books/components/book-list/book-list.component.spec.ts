e955ba1819175c261c2a069e37748abfb2a9cd37
Maciej Małecki <external.maciej.malecki@kuehne-nagel.com>
2023-06-02 14:59:17 +0200

    dom tests

--- projects/bookstore/src/app/books/components/book-list/book-list.component.spec.ts
+++ projects/bookstore/src/app/books/components/book-list/book-list.component.spec.ts
@@ -1,28 +1,75 @@
 import {BookListComponent} from "./book-list.component";
 import {BookService} from "../../services/book.service";
+import {ComponentFixture, TestBed} from "@angular/core/testing";
 
 
 describe("BookListComponent", () => {
 
   let component: BookListComponent;
 
-  let service: BookService;
+  describe("[DOM]", () => {
+    let fixture: ComponentFixture<BookListComponent>;
+    let nativeElement: any;
 
-  beforeEach(() => {
-    service = new BookService();
-    component = new BookListComponent(service);
+    // test utility functions
+    // nouns
+    const bookList = () => nativeElement.querySelectorAll('li.clickable') as NodeList;
+    const editor = () => nativeElement.querySelector('#editor') as HTMLElement;
+    const bookAt = (position: number) => bookList().item(position) as HTMLLIElement;
+    // verbs
+    const clickBookAt = (position: number) => bookAt(position).dispatchEvent(new MouseEvent('click'));
+    const detectChanges = () => fixture.detectChanges();
+
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        declarations: [BookListComponent],
+        providers: [BookService]
+      }).compileComponents();
+    });
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(BookListComponent);
+      component = fixture.componentInstance;
+      nativeElement = fixture.nativeElement;
+      detectChanges();
+    });
+
+    it('can be created', () => {
+      expect(component).toBeTruthy();
+    })
+
+    it('shows an editor once a book is clicked', () => {
+      // given
+      expect(bookList().length).toEqual(3);
+      expect(editor()).toBeFalsy();
+      // when
+      clickBookAt(1);
+      detectChanges();
+      // then
+      expect(component.selectedBook).toBeTruthy();
+      expect(editor()).toBeTruthy();
+    });
   });
 
-  it('has no selected book initially', () => {
-    expect(component.selectedBook).toBeFalsy();
+  describe("[class]", () => {
+
+    let service: BookService;
+
+    beforeEach(() => {
+      service = new BookService();
+      component = new BookListComponent(service);
+    });
+
+    it('has no selected book initially', () => {
+      expect(component.selectedBook).toBeFalsy();
+    });
+
+    it('has three books on the list', () => {
+      expect(component.books).toHaveSize(3);
+    })
+
+    it('has books the same as in service', () => {
+      expect(component.books).toEqual(service.getBooks());
+    });
   });
-
-  it('has three books on the list', () => {
-    expect(component.books).toHaveSize(3);
-  })
-
-  it('has books the same as in service', () => {
-    expect(component.books).toEqual(service.getBooks());
-  });
-
 });
