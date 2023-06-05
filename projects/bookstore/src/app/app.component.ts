a414f7cde4e2d7260a16aa146200ec81f13d9f5d
Maciej Małecki <external.maciej.malecki@kuehne-nagel.com>
2023-06-05 13:26:19 +0200

    view child

--- projects/bookstore/src/app/app.component.ts
+++ projects/bookstore/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  @ViewChild("editor")
+  editor: ElementRef<HTMLInputElement> | undefined;
+  // editor!: ElementRef<HTMLInputElement>
+
+  // title: string | undefined = 'bookstore';
   title = 'bookstore';
 
   onBlur() {
     console.log('blur');
+  }
+
+  updateTitle() {
+    if(this.editor) {
+      this.title = this.editor.nativeElement.value;
+    }
+    // this.title = this.editor?.nativeElement.value;
   }
 }
