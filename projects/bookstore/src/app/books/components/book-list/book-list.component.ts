cbdeca10651fdb281c5a8b48af5974177642050f
Maciej Małecki <external.maciej.malecki@kuehne-nagel.com>
2023-06-05 15:06:41 +0200

    proper save for editor

--- projects/bookstore/src/app/books/components/book-list/book-list.component.ts
+++ projects/bookstore/src/app/books/components/book-list/book-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {BooksService} from "../../services/books.service";
 import {Book} from "../../model/book";
 
@@ -10,19 +10,34 @@ import {Book} from "../../model/book";
 })
 export class BookListComponent {
 
+  @ViewChild("title")
+  titleElement!: ElementRef<HTMLInputElement>;
+
+  @ViewChild("author")
+  authorElement!: ElementRef<HTMLInputElement>;
+
+  @ViewChild("description")
+  descriptionElement!: ElementRef<HTMLTextAreaElement>;
+
   selectedBook: Book | null = null;
 
   books: Book[] = [];
 
   constructor(private readonly booksService: BooksService) {
     this.books = this.booksService.getBooks();
-
   }
 
   save(): void {
     if (this.selectedBook) {
-      this.booksService.save(this.selectedBook);
+      const book: Book = {
+        id: this.selectedBook.id,
+        title: this.titleElement.nativeElement.value,
+        author: this.authorElement.nativeElement.value,
+        description: this.descriptionElement.nativeElement.value
+      };
+      this.booksService.save(book);
       this.selectedBook = null;
+      this.books = this.booksService.getBooks();
     }
   }
 }
