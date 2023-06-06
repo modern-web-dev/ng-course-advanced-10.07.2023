6abe3c710b655ecfe6523be91dea5af47660e873
Maciej Małecki <external.maciej.malecki@kuehne-nagel.com>
2023-06-06 10:55:49 +0200

    Router

--- projects/bookstore/src/app/app.module.ts
+++ projects/bookstore/src/app/app.module.ts
@@ -3,11 +3,8 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {GidcRootModule} from "@gidc/pali/root-module";
-import {MaterialModule} from "./shared/material.module";
 import {BooksModule} from "./books/books.module";
-import {BooksService} from "./books/services/books.service";
 
 @NgModule({
   declarations: [
---
+++ projects/bookstore/src/app/core/core.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
