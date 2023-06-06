66918e16d64750faa7385f93376b229b808b9b42
Maciej Małecki <external.maciej.malecki@kuehne-nagel.com>
2023-06-06 10:31:40 +0200

    mocked book service

--- projects/bookstore/src/app/books/components/book-list/book-list.component.spec.ts
+++ projects/bookstore/src/app/books/components/book-list/book-list.component.spec.ts
@@ -2,7 +2,7 @@ import {BookListComponent} from "./book-list.component";
 import {BooksService} from "../../services/books.service";
 import {ComponentFixture, TestBed} from "@angular/core/testing";
 import {MaterialModule} from "../../../shared/material.module";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {Book} from "../../model/book";
 
 
 describe('BookListComponent', () => {
@@ -13,13 +13,35 @@ describe('BookListComponent', () => {
 
     let fixture: ComponentFixture<BookListComponent>;
     let nativeElement: any;
-    let bookService: BooksService;
+    let booksServiceMock: any;
+    let books: Book[];
 
     beforeEach(async () => {
+      books = [{
+        id: 1,
+        title: 'Solaris',
+        author: 'Stanisław Lem',
+        description: 'Solaris chronicles the ultimate futility of attempted communications with the extraterrestrial life inhabiting a distant alien planet named Solaris. The planet is almost completely covered with an ocean of gel that is revealed to be a single, planet-encompassing entity. Terran scientists conjecture it is a living and a sentient being, and attempt to communicate with it.'
+      }, {
+        id: 2,
+        title: '2001: A Space Odyssey',
+        author: 'Aurthur C. Clarke',
+        description: 'A mysterious alien civilization uses a tool with the appearance of a large crystalline monolith to investigate worlds across the galaxy and, if possible, to encourage the development of intelligent life. The book shows one such monolith appearing in prehistoric Africa, 3 million years ago (in the movie, 4 mya), where it inspires a starving group of hominids to develop tools. The hominids use their tools to kill animals and eat meat, ending their starvation. They then use the tools to kill a leopard preying on them; the next day, the main ape character, Moon-Watcher, uses a club to kill the leader of a rival tribe. The book suggests that the monolith was instrumental in awakening intelligence.'
+      }, {
+        id: 3,
+        title: 'Ubik',
+        author: 'Phillip K. Dick',
+        description: 'By the year 1992, humanity has colonized the Moon and psychic powers are common. The protagonist, Joe Chip, is a debt-ridden technician working for Runciter Associates, a "prudence organization" employing "inertials"—people with the ability to negate the powers of telepaths and "precogs"—to enforce the privacy of clients. The company is run by Glen Runciter, assisted by his deceased wife Ella who is kept in a state of "half-life", a form of cryonic suspension that allows the deceased limited consciousness and ability to communicate. While consulting with Ella, Runciter discovers that her consciousness is being invaded by another half-lifer named Jory Miller.'
+      }];
+      booksServiceMock = {
+        getBooks: jasmine.createSpy().and.returnValue(books),
+        save: jasmine.createSpy()
+      };
+
       await TestBed.configureTestingModule({
         declarations: [BookListComponent],
         imports: [MaterialModule],
-        providers: [BooksService]
+        providers: [{ provide: BooksService, useValue: booksServiceMock }]
       }).compileComponents();
     });
 
@@ -45,7 +67,6 @@ describe('BookListComponent', () => {
       fixture = TestBed.createComponent(BookListComponent);
       testedComponent = fixture.componentInstance;
       nativeElement = fixture.nativeElement;
-      bookService = TestBed.inject(BooksService);
       detectChanges();
     });
 
@@ -64,7 +85,7 @@ describe('BookListComponent', () => {
     it('shows an editor once a book is clicked', () => {
       // given
       const position = 1;
-      const book = bookService.getBooks()[position];
+      const book = booksServiceMock.getBooks()[position];
       // when
       clickBookAt(position);
       detectChanges();
@@ -95,7 +116,7 @@ describe('BookListComponent', () => {
       // given
       const position = 1;
       clickBookAt(position);
-      const bookBeforeChange = bookService.getBooks()[position];
+      const bookBeforeChange = booksServiceMock.getBooks()[position];
       detectChanges();
       expect(editor()).toBeTruthy();
       expect(testedComponent.selectedBook).toBeTruthy();
@@ -105,12 +126,11 @@ describe('BookListComponent', () => {
       // then
       expect(editor()).toBeFalsy();
       expect(testedComponent.selectedBook).toBeNull();
-      expect(bookService.getBooks()[position]).toEqual(bookBeforeChange);
+      expect(booksServiceMock.getBooks()[position]).toEqual(bookBeforeChange);
     });
 
     it('saves a modified book', () => {
       // given
-      spyOn(bookService, 'save').and.callThrough();
       const position = 1;
       clickBookAt(position);
       const bookBeforeChange = testedComponent.books[position];
@@ -129,7 +149,7 @@ describe('BookListComponent', () => {
       // expect(editor()).toBeFalsy();
       const modifiedBook = testedComponent.books[position];
       expect(modifiedBook).toBeTruthy();
-      expect(bookService.save).toHaveBeenCalledWith({
+      expect(booksServiceMock.save).toHaveBeenCalledWith({
         id: bookBeforeChange.id,
         title: newTitle,
         author: newAuthor,
