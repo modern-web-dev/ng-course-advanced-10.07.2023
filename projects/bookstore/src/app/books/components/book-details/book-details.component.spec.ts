193f50c75b774b27e4295147ff040d98a938f8e4
Maciej Małecki <external.maciej.malecki@kuehne-nagel.com>
2023-06-06 14:39:01 +0200

    input binding propagation test

--- projects/bookstore/src/app/books/components/book-details/book-details.component.spec.ts
+++ projects/bookstore/src/app/books/components/book-details/book-details.component.spec.ts
@@ -3,10 +3,13 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BookDetailsComponent } from './book-details.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {MaterialModule} from "../../../shared/material.module";
+import {Book} from "../../model/book";
+import {SimpleChange} from "@angular/core";
 
 describe('BookDetailsComponent', () => {
   let component: BookDetailsComponent;
   let fixture: ComponentFixture<BookDetailsComponent>;
+  let nativeElement: any;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,10 +20,47 @@ describe('BookDetailsComponent', () => {
 
     fixture = TestBed.createComponent(BookDetailsComponent);
     component = fixture.componentInstance;
+    nativeElement = fixture.nativeElement;
     fixture.detectChanges();
   });
 
+  // test utility functions
+  // "nouns"
+  const editor = () => nativeElement.querySelector('#editor') as HTMLElement;
+  const saveButton = () => nativeElement.querySelector("button#save") as HTMLButtonElement;
+  const cancelButton = () => nativeElement.querySelector("button#cancel") as HTMLButtonElement;
+  const inputElementOfId = (id: string) => nativeElement.querySelector(`input#${id}`) as HTMLInputElement;
+  const titleElement = () => inputElementOfId("title") as HTMLInputElement;
+  const authorElement = () => inputElementOfId("author") as HTMLInputElement;
+  const descriptionElement = () => nativeElement.querySelector("textarea#description") as HTMLTextAreaElement;
+  // "verbs"
+  const clickCancel = () => cancelButton().dispatchEvent(new MouseEvent('click'));
+  const clickSave = () => saveButton().dispatchEvent(new MouseEvent('click'));
+  const detectChanges = () => fixture.detectChanges();
+  const editField = (fieldElement: HTMLInputElement | HTMLTextAreaElement, value: string) => {
+    fieldElement.value = value;
+    fieldElement.dispatchEvent(new Event('input'));
+  }
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should fill with book once book is set', () => {
+    // given
+    const book: Book = {
+      id: 123,
+      title: 'foo',
+      author: 'bar',
+      description: 'some description'
+    }
+    // when
+    component.book = book;
+    component.ngOnChanges({ book: new SimpleChange(null, book, true)});
+    detectChanges();
+    // then
+    expect(titleElement().value).toEqual(book.title);
+    expect(authorElement().value).toEqual(book.author);
+    expect(descriptionElement().value).toEqual(book.description);
   });
 });
