e9a0dd92ad1fb766a4dd67acaa27b53c4335a12c
Maciej Małecki <external.maciej.malecki@kuehne-nagel.com>
2023-07-10 16:15:40 +0200

    save tested

--- projects/bookstore/src/app/books/components/book-list/book-list.component.spec.ts
+++ projects/bookstore/src/app/books/components/book-list/book-list.component.spec.ts
@@ -21,7 +21,7 @@ describe('BookListComponent', () => {
         imports: [MaterialModule, BrowserAnimationsModule],
         providers: [BooksService]
       })
-      .compileComponents();
+        .compileComponents();
     });
 
     // test utility functions
@@ -30,10 +30,17 @@ describe('BookListComponent', () => {
     const editorButtons = () => nativeElement.querySelector('#editor-buttons') as HTMLDivElement;
     const booksList = () => nativeElement.querySelectorAll("li") as NodeList;
     const bookAt = (position: number) => booksList().item(position) as HTMLLIElement;
+    const cancelButton = () => nativeElement.querySelector('#cancel') as HTMLButtonElement;
+    const saveButton = () => nativeElement.querySelector('#save') as HTMLButtonElement;
+    const titleElement = () => nativeElement.querySelector('input#title') as HTMLInputElement;
+    const authorElement = () => nativeElement.querySelector('input#author') as HTMLInputElement;
+    const descriptionElement = () => nativeElement.querySelector('textarea#description') as HTMLTextAreaElement;
 
     // "verbs"
     const detectChanges = () => fixture.detectChanges();
     const clickAt = (element: HTMLElement) => element.dispatchEvent(new MouseEvent('click'));
+    const editField = (element: HTMLInputElement | HTMLTextAreaElement, value: string) =>
+      element.value = value;
 
     beforeEach(() => {
       fixture = TestBed.createComponent(BookListComponent);
@@ -67,6 +74,58 @@ describe('BookListComponent', () => {
 
     it('displays three books', () => {
       expect(booksList().length).toEqual(3);
+    });
+
+    it('closes an editor when cancel is clicked', () => {
+      // given
+      clickAt(bookAt(1));
+      detectChanges();
+      expect(editor()).toBeTruthy();
+      expect(editorButtons()).toBeTruthy();
+      // when
+      clickAt(cancelButton());
+      detectChanges();
+      // then
+      expect(editor()).toBeFalsy();
+      expect(editorButtons()).toBeFalsy();
+    });
+
+    it('closes an editor when save is clicked', () => {
+      // given
+      clickAt(bookAt(1));
+      detectChanges();
+      expect(editor()).toBeTruthy();
+      expect(editorButtons()).toBeTruthy();
+      // when
+      clickAt(saveButton());
+      detectChanges();
+      // then
+      expect(editor()).toBeFalsy();
+      expect(editorButtons()).toBeFalsy();
+    });
+
+    it('saves a changed book and closes the editor once save is clicked', () => {
+      // given
+      const oldBooks = testedComponent.books;
+      clickAt(bookAt(1));
+      detectChanges(); // selectedBook is not null now
+      // when
+      editField(titleElement(), "foo");
+      editField(authorElement(), "bar");
+      editField(descriptionElement(), "1234");
+      clickAt(saveButton());
+      detectChanges(); // no book is selected now, book list is refreshed
+      // then
+      expect(editor()).toBeFalsy();
+      expect(editorButtons()).toBeFalsy();
+      const newBooks = testedComponent.books;
+      expect(newBooks).not.toEqual(oldBooks);
+      expect(newBooks[1]).toEqual({
+        id: 2,
+        title: 'foo',
+        author: 'bar',
+        description: '1234'
+      });
     });
   });
 
