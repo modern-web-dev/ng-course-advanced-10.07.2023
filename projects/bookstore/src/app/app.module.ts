d05e46d78706cb381e42669af0f02a1c32955718
Maciej Małecki <external.maciej.malecki@kuehne-nagel.com>
2023-07-11 10:57:50 +0200

    router

--- projects/bookstore/src/app/app.module.ts
+++ projects/bookstore/src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {GidcRootModule} from "@gidc/pali/root-module";
 import {BooksModule} from "./books/books.module";
+import {CoreModule} from "./core/core.module";
 
 @NgModule({
   declarations: [
@@ -15,10 +16,12 @@ import {BooksModule} from "./books/books.module";
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    CoreModule,
     BooksModule,
     GidcRootModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
